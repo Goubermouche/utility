f3a0088dc0844c0aaf1968876f915cbdc5b6bc62
Šimon Tupý <simontupy64@gmail.com>
2024-07-07 16:54:13 +0200

    Cleanup.

--- utility/types.h
+++ utility/types.h
@@ -93,24 +93,6 @@ namespace utility {
 		std::chrono::duration<f64, std::micro> m_elapsed = std::chrono::high_resolution_clock::duration::zero();
 	};
 
-	class byte {
-	public:
-		constexpr byte() : m_value(0) {}
-		constexpr byte(u8 value) : m_value(value) {}
-
-		[[nodiscard]] constexpr operator u8() const { return m_value; }
-		[[nodiscard]] constexpr auto operator<=>(const byte& other) const->std::strong_ordering = default;
-		[[nodiscard]] constexpr auto operator~() const -> byte { return ~m_value; }
-		[[nodiscard]] constexpr auto operator&(const byte& other) const -> byte { return m_value & other.m_value; }
-		[[nodiscard]] constexpr auto operator|(const byte& other) const -> byte { return m_value | other.m_value; }
-		[[nodiscard]] constexpr auto operator^(const byte& other) const -> byte { return m_value ^ other.m_value; }
-		[[nodiscard]] constexpr auto operator<<(i32 shift) const -> byte { return static_cast<u8>(m_value << shift); }
-		[[nodiscard]] constexpr auto operator>>(i32 shift) const -> byte { return m_value >> shift; }
-		[[nodiscard]] friend auto operator+(const byte& a, const byte& b) -> byte { return a.m_value + b.m_value; }
-	private:
-		u8 m_value;
-	};
-
 	template<typename a, typename b = a>
 	auto min(a left, b right) {
 		return left > right ? right : left;
@@ -211,14 +193,6 @@ namespace utility {
 			return -9223372036854775808ll;
 		}
 	};
-
-	inline auto sign_extend(u64 x, u8 x_bits, u8 n) -> u64 {
-		if((x >> (x_bits - 1)) & 1) {
-			x |= ((limits<u64>::max()) << x_bits) >> n;
-		}
-
-		return x;
-	}
 } // namespace utility
 
 #define EXPAND(__x) __x
